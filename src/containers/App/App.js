6f5b708733fbfe6d73415aadd974251a0330444e
Timur Kostenko <ktim8168@mail.ru>
2019-09-12 00:16:50 +0300

    First commit

---
+++ src/containers/App/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import classNames from './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className={ classNames.App }>
+        <p>Test</p>
+      </div>
+    );
+  }
+}
+
+export default App;
